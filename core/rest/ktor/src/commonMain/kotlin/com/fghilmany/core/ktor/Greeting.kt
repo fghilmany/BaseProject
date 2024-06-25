5597cee4dc6fb2fff32bc7dd95fdd2cced6efe2a
fghilmany <ceeros99@gmail.com>
2024-06-25 10:12:21 +0700

    feat: Init ktor

---
+++ core/rest/ktor/src/commonMain/kotlin/com/fghilmany/core/ktor/Greeting.kt
@@ -0,0 +1,9 @@
+package com.fghilmany.core.ktor
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
